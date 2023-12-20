bf39a951c929970f6f8cc3d10d8ed4d5bca3198b
stevenarbo3 <steven.arbo@icloud.com>
2023-12-19 21:15:54 -0500

    using class

--- madlib.py
+++ madlib.py
@@ -1,10 +1,15 @@
-adj = input("Adjective: ")
-verb1 = input("Verb: ")
-verb2 = input("Verb: ")
-famous_person = input("Famous Person: ")
+class Madlib:
 
-madlib = f"Computer programming is so {adj}! It makes me so excited all the 
-time because I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+  adj = input("Adjective: ")
+  verb = input("Verb: ")
+  famous_person = input("Famous Person: ")
 
-print(madlib)
+  line = f"Computer programming is so {adj}! It makes me so excited all the time because I love to {verb}. Stay hydrated like you are {famous_person}"
+  
+  def printMadlib(self,line):
+      print(line)
+
+madlib1 = Madlib()
+
+madlib1.printMadlib(madlib1.line)
 
